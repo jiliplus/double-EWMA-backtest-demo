05576fd8ed49418b2f236846168368124637f9f2
aQuaYi.com <6028869+aQuaYi@users.noreply.github.com>
2020-06-03 11:19:13 +0800

    没有止损的策略

--- main.go
+++ main.go
@@ -45,8 +45,8 @@ func main() {
 	// 启动 TickBarService
 	backtest.TickBarService(context.TODO(), pubSub, interval)
 
-	srcName := "./btcusdt.sqlite3"
-	// srcName := "./binance.sqlite3"
+	// srcName := "./btcusdt.sqlite3"
+	srcName := "./binance.sqlite3"
 	//
 	db := openToMemory(srcName)
 	defer db.Close()
